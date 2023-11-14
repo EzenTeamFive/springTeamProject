bf01b5fff280b49448c9e265576a7e793fb2a031
yeon <134473033+halsooitda@users.noreply.github.com>
2023-11-14 16:28:41 +0900

    Update

--- gitTest/src/main/java/com/test/git/domain/BoardVO.java
+++ gitTest/src/main/java/com/test/git/domain/BoardVO.java
@@ -5,19 +5,5 @@ public class BoardVO {
 	private String title;
 	
 	private String content;
-<<<<<<< HEAD
-	//¼öÁ¤!!
-	// 2023 11 13 ±è¹Ì¼ö Ãß°¡¼öÁ¤??
-=======
-	//testtesttest!
 
-// 23 - 11-14 ±è¹Ì¼ö test 
-
-
-	
-<<<<<<< HEAD
->>>>>>> 4ad93e95527ab3bf65e121b24a46a371f3ec85e8
-=======
-	//ì„œì—° ì§€ì˜ ë³‘í•©
->>>>>>> 2dc7375e7533a29d15e7faceaedfa4dd06fc1beb
 }
